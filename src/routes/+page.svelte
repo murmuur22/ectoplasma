3dcd16af29fa2af2cbcdd1be03cbfedb5c58ce3d
murmuur22 <69942927+murmuurr@users.noreply.github.com>
2024-03-21 16:22:50 -0400

    hid cursor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { spring } from 'svelte/motion';
 
-  let display = "absolute";
+  let position = "absolute";
 
   let X = 0;
   let Y = 0;
@@ -26,17 +26,29 @@
     cursor.style.left = $coords.x - cursorHalfWidth + "px";
     cursor.style.top = $coords.y - cursorHalfWidth + "px";
 
+    cursor.style.position = position;
+
   }
 
 </script>
 
+<!-- svelte-ignore a11y-no-static-element-interactions -->
 <div 
   class="
     relative overflow-hidden
-    h-screen w-full flex items-center justify-center 
+    h-screen w-full flex items-center justify-center cursor-none
     bg-[url(/images/Ectoplasma_invisible.jpg)] bg-cover bg-center bg-fixed
   
   "
+  on:mouseenter={(e) => {
+    cursor.style.display = "block";
+    console.log(cursor.style.display);
+
+  }}
+  on:mouseleave={(e) => {
+    cursor.style.display = "none";
+    console.log(cursor.style.display);
+  }}
 >
   <div
     bind:this={cursor}
